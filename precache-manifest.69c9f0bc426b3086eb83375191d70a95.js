b719cf0565fad218edd31eeaadd7561880a5b9d2
Jacob <jkabshear3@gmail.com>
2019-09-27 12:55:11 -0400

    Updates

---
+++ precache-manifest.69c9f0bc426b3086eb83375191d70a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019336c9af4a73b5fc94b275582bb795",
+    "url": "/hangmanjs/index.html"
+  },
+  {
+    "revision": "49b574c2a453d30c683c",
+    "url": "/hangmanjs/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "94430a47ead2b66d9686",
+    "url": "/hangmanjs/static/css/main.54adb256.chunk.css"
+  },
+  {
+    "revision": "49b574c2a453d30c683c",
+    "url": "/hangmanjs/static/js/2.30de16c9.chunk.js"
+  },
+  {
+    "revision": "94430a47ead2b66d9686",
+    "url": "/hangmanjs/static/js/main.1902aedb.chunk.js"
+  },
+  {
+    "revision": "2dad1610779e5f0e66e3",
+    "url": "/hangmanjs/static/js/runtime~main.afa3e651.js"
+  }
+]);
